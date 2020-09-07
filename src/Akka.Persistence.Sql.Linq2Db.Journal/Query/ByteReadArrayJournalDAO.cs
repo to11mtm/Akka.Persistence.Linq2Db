f992213829d0b853351488b5d586055a6500b929
Andrew El <laingas@gmail.com>
2020-09-07 11:24:06 -0400

    query wip

---
+++ src/Akka.Persistence.Sql.Linq2Db.Journal/Query/ByteReadArrayJournalDAO.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Persistence.Sql.Linq2Db.Journal.Config;
+using Akka.Persistence.Sql.Linq2Db.Journal.DAO;
+using Akka.Persistence.Sql.Linq2Db.Journal.Types;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+using LanguageExt;
+using LinqToDB;
+using LinqToDB.Async;
+using LinqToDB.Data;
+
+namespace Akka.Persistence.Sql.Linq2Db.Journal.Query
+{
+    public class EventsByTagResponseItem
+    {
+        public IPersistentRepresentation Repr { get; set; }
+        public ImmutableHashSet<string> Tags { get; set; }
+        public long SequenceNr { get; set; }
+    }
+    public interface IReadJournalDAO : IJournalDaoWithReadMessages
+    {
+        Source<string, NotUsed> allPersistenceIdsSource(long max);
+
+        Source<Util.Try<(IPersistentRepresentation, IImmutableSet<string>, long)>, NotUsed>
+            eventsByTag(string tag, long offset, long maxOffset, long max);
+
+        Source<long, NotUsed> journalSequence(long offset,
+            long limit);
+
+        Task<long> maxJournalSequence();
+    }
+    public abstract class BaseByteReadArrayJournalDAO :BaseJournalDaoWithReadMessages , IReadJournalDAO
+    {
+        private AkkaPersistenceDataConnectionFactory _connectionFactory;
+        private bool includeDeleted;
+        private ReadJournalConfig _readJournalConfig;
+        private FlowPersistentReprSerializer<JournalRow> _serializer;
+
+        protected BaseByteReadArrayJournalDAO(IAdvancedScheduler ec,
+            IMaterializer mat,
+            AkkaPersistenceDataConnectionFactory connectionFactory,
+            ReadJournalConfig readJournalConfig,
+            FlowPersistentReprSerializer<JournalRow> serializer) : base(ec, mat,
+            connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+            
+            _readJournalConfig = readJournalConfig;
+            includeDeleted = readJournalConfig.IncludeDeleted;
+            _serializer = serializer;
+        }
+
+        protected IQueryable<JournalRow> baseQuery(DataConnection connection)
+        {
+            return connection.GetTable<JournalRow>()
+                .Where(jr =>
+                    includeDeleted == false || (jr.deleted == false));
+        }
+        public Source<string, NotUsed> allPersistenceIdsSource(long max)
+        {
+            using (var db = _connectionFactory.GetConnection())
+            {
+                var maxTake = MaxTake(max);
+                return Source.From(baseQuery(db)
+                    .Select(r=>r.persistenceId).Distinct()
+                    .Take(maxTake));
+            }
+            
+        }
+
+        private static int MaxTake(long max)
+        {
+            int maxTake;
+            if (max > Int32.MaxValue)
+            {
+                maxTake = Int32.MaxValue;
+            }
+            else
+            {
+                maxTake = (int) max;
+            }
+
+            return maxTake;
+        }
+
+        public Source<
+            Util.Try<(IPersistentRepresentation, IImmutableSet<string>, long)>,
+            NotUsed> eventsByTag(string tag, long offset, long maxOffset,
+            long max)
+        {
+            var separator = _readJournalConfig.PluginConfig.TagSeparator;
+            var maxTake = MaxTake(max);
+            using (var conn = _connectionFactory.GetConnection())
+            {
+                return Source.FromObservable(conn.GetTable<JournalRow>()
+                        .Where(r => r.tags.Contains(tag))
+                        .OrderBy(r => r.ordering)
+                        .Where(r =>
+                            r.ordering > offset && r.ordering <= maxOffset)
+                        .Take(maxTake).ToAsyncEnumerable().ToObservable())
+                    .Via(perfectlyMatchTag(tag, separator))
+                    .Via(_serializer.DeserializeFlow());
+
+            }
+        }
+
+        private Flow<JournalRow, JournalRow,NotUsed> perfectlyMatchTag(string tag,
+            string separator)
+        {
+            
+            return Flow.Create<JournalRow>().Where(r =>
+                (r.tags ?? "")
+                .Split(new[] {separator}, StringSplitOptions.RemoveEmptyEntries)
+                .Any(t => t.Contains(tag)));
+        }
+
+        public override Source<Util.Try<ReplayCompletion>, NotUsed> Messages(DataConnection dc, string persistenceId, long fromSequenceNr,
+            long toSequenceNr, long max)
+        {
+            var toTake = MaxTake(max);
+            using (var conn = _connectionFactory.GetConnection())
+            {
+                return Source.From(
+                        baseQuery(conn)
+                            .Where(r => r.persistenceId == persistenceId
+                                        && r.sequenceNumber >= fromSequenceNr
+                                        && r.sequenceNumber <= toSequenceNr)
+                            .OrderBy(r => r.sequenceNumber)
+                            .Take(toTake)).Via(_serializer.DeserializeFlow())
+                    .Select(
+                        t =>
+                        {
+                            try
+                            {
+                                var val = t.Get();
+                                return new Util.Try<ReplayCompletion>(
+                                    new ReplayCompletion()
+                                    {
+                                        repr = val.Item1, SequenceNr = val.Item3
+                                    });
+                            }
+                            catch (Exception e)
+                            {
+                                return new Util.Try<ReplayCompletion>(e);
+                            }
+                        });
+
+            }
+            
+        }
+
+        public Source<long, NotUsed> journalSequence(long offset, long limit)
+        {
+            var maxTake = MaxTake(limit);
+            using (var conn = _connectionFactory.GetConnection())
+            {
+                return Source.From(conn.GetTable<JournalRow>()
+                    .Where(r => r.ordering > offset).Select(r => r.ordering)
+                    .OrderBy(r=>r).Take(maxTake));
+            }
+        }
+
+        public async Task<long> maxJournalSequence()
+        {
+            using (var db = _connectionFactory.GetConnection())
+            {
+                return await db.GetTable<JournalRow>()
+                    .Select(r => r.ordering).FirstOrDefaultAsync();
+            }
+        }
+    }
+
+    public class ByteArrayReadJournalDao : BaseByteReadArrayJournalDAO
+    {
+        public ByteArrayReadJournalDao(IAdvancedScheduler ec, IMaterializer mat, AkkaPersistenceDataConnectionFactory connectionFactory, ReadJournalConfig readJournalConfig, FlowPersistentReprSerializer<JournalRow> serializer) : base(ec, mat, connectionFactory, readJournalConfig, serializer)
+        {
+        }
+    }
+}
---
+++ src/Akka.Persistence.Sql.Linq2Db.Journal/Query/Linq2DbReadJournal.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Akka.Actor;
+using Akka.Event;
+using Akka.Persistence.Query;
+using Akka.Persistence.Sql.Linq2Db.Journal.Config;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+using Akka.Util.Internal;
+
+namespace Akka.Persistence.Sql.Linq2Db.Journal.Query
+{
+    public class MissingElements
+    {
+        public IImmutableList<Range>
+    }
+
+    public class AssumeMaxOrderingIdTimerKey
+    {
+        public static AssumeMaxOrderingIdTimerKey Instance => new AssumeMaxOrderingIdTimerKey();
+    }
+    public class JournalSequenceActor : ActorBase, IWithTimers
+    {
+        private JournalSequenceRetrievalConfig _config;
+        private IReadJournalDAO _readJournalDao;
+        private TimeSpan queryDelay;
+        private int maxTries;
+        private ILoggingAdapter _log;
+        private ActorMaterializer _mat;
+        public ITimerScheduler Timers { get; set; }
+        public JournalSequenceActor(IReadJournalDAO readJournalDao,
+            JournalSequenceRetrievalConfig config)
+        {
+            _mat = ActorMaterializer.Create(Context,
+                ActorMaterializerSettings.Create(Context.System),
+                "linq2db-query");
+            _readJournalDao = readJournalDao;
+            _config = config;
+            queryDelay = config.QueryDelay;
+            maxTries = config.MaxTries;
+            _log = Context.GetLogger();
+        }
+
+        protected bool receive(object message)
+        {
+            return receive(message, 0, new Dictionary<int, MissingElements>(), 0, queryDelay);
+        }
+
+        protected bool receive(object message, long currentMaxOrdering,
+            Dictionary<int, MissingElements> missingByCounter,
+            int moduloCounter, TimeSpan previousDelay)
+        {
+            if (message is ScheduleAssumeMaxOrderingId s)
+            {
+                var delay = queryDelay * maxTries;
+                Timers.StartSingleTimer(AssumeMaxOrderingIdTimerKey.Instance, new AssumeMaxOrderingId(s.MaxInDatabase),delay);
+            }
+            else if (message is AssumeMaxOrderingId a)
+            {
+                if (currentMaxOrdering < a.Max)
+                {
+                    Become((o => receive(o, maxTries, missingByCounter,
+                        moduloCounter, previousDelay)));
+                }
+            }
+            else if (message is GetMaxOrderingId)
+            {
+                Sender.Tell(new MaxOrderingId(currentMaxOrdering));
+            }
+            else if (message is QueryOrderingIds)
+            {
+                _readJournalDao
+                    .journalSequence(currentMaxOrdering, _config.BatchSize)
+                    .RunWith(Sink.Seq<long>(), _mat).PipeTo(Self, sender: Self,
+                        success: res =>
+                            new NewOrderingIds(currentMaxOrdering, res));
+            }
+            else if (message is NewOrderingIds nids)
+            {
+                if (nids.MaxOrdering < currentMaxOrdering)
+                {
+                    Self.Tell(new QueryOrderingIds());
+                }
+                else
+                {
+                    findGaps(nids.Elements, currentMaxOrdering,
+                        missingByCounter, moduloCounter);
+                }
+            }
+            else if (message is Status.Failure t)
+            {
+                var newDelay =
+                    _config.MaxBackoffQueryDelay.Min(previousDelay * 2);
+                if (newDelay == _config.MaxBackoffQueryDelay)
+                {
+                    _log.Warning(
+                        "Failed to query max Ordering ID Because of {0}, retrying in {1}",
+                        t, newDelay);
+                }
+
+                scheduleQuery(newDelay);
+                Context.Become(o => receive(o, currentMaxOrdering,
+                    missingByCounter, moduloCounter, newDelay));
+            }
+        }
+
+        private void findGaps(IImmutableList<long> elements,
+            long currentMaxOrdering,
+            Dictionary<int, MissingElements> missingByCounter, int moduloCounter)
+        {
+            var givenUp =
+                missingByCounter.GetOrElse(moduloCounter,
+                    new MissingElements());
+            elements.Aggregate((currentMaxOrdering, currentMaxOrdering, missin))
+        }
+
+        protected override bool Receive(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PreStart()
+        {
+            var self = Self;
+            self.Tell(new QueryOrderingIds());
+            _readJournalDao.maxJournalSequence().ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    _log.Info(
+                        "Failed to recover fast, using event-by-event recovery instead",
+                        t.Exception);
+                }
+                else if (t.IsCompleted)
+                {
+                    self.Tell(new ScheduleAssumeMaxOrderingId(t.Result));
+                }
+            });
+            base.PreStart();
+        }
+
+        
+    }
+
+    public class NewOrderingIds
+    {
+        public long MaxOrdering { get; }
+        public object Elements { get; set; }
+        
+        public NewOrderingIds(in long currentMaxOrdering, object res)
+        {
+            MaxOrdering = currentMaxOrdering;
+            Elements = res;
+        }
+    }
+
+    public class AssumeMaxOrderingId
+    {
+        public AssumeMaxOrderingId(long max)
+        {
+            Max = max;
+        }
+
+        public long Max { get; set; }
+    }
+
+    public class GetMaxOrderingId
+    {
+        
+    }
+    public class MaxOrderingId
+    {
+        public MaxOrderingId(long max)
+        {
+            Max = max;
+        }
+
+        public long Max { get; set; }
+    }
+    public class ScheduleAssumeMaxOrderingId
+    {
+        public ScheduleAssumeMaxOrderingId(long maxInDatabase)
+        {
+            MaxInDatabase = maxInDatabase;
+        }
+
+        public long MaxInDatabase { get; set; }
+    }
+
+    public class QueryOrderingIds
+    {
+        
+    }
+    public class Linq2DbReadJournal :  
+        IPersistenceIdsQuery,
+        ICurrentPersistenceIdsQuery,
+        IEventsByPersistenceIdQuery,
+        ICurrentEventsByPersistenceIdQuery,
+        IEventsByTagQuery,
+        ICurrentEventsByTagQuery,
+        IAllEventsQuery,
+        ICurrentAllEventsQuery
+    {
+        private IActorRef journalSequenceActor;
+        public Linq2DbReadJournal(ExtendedActorSystem system, Config config)
+        {
+            system.ActorOf()
+        }
+
+        
+    }
+}
